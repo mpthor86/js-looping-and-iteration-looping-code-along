7efe3f9f12a0f23a3645840a271133fcc7bbe91a
michael <mpthornton86@gmail.com>
2021-01-27 11:36:34 -0700

    commit tree 90137feb2926d975a83eb04d207fe81d723055df

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(col, event) {
+    let thanks = [];
+    for (let i = 0; i < col.length; i++) {
+    thanks.push(`Thank you, ${col[i]}, for the wonderful ${event} gift!`);
+    }
+    return thanks;
+}
+
+function countDown(count) {
+while(count > -1){
+    console.log(count--);
+    }
+}
